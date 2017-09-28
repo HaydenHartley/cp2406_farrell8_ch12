e6ede425cc0027dec86fd1c8e184f24eded4d6c2
HaydenHartley <hhart30@hotmail.com>
2017-09-29 09:55:00 +1000

    Finish Prac 12

--- CodeInFigures/UninitializedVariableTest.java
+++ CodeInFigures/UninitializedVariableTest.java
@@ -1,20 +1,17 @@
 import java.util.Scanner;
-class UninitializedVariableTest
-{
-   static void main(String[] args)
-   {
-      int x;
-      Scanner input = new Scanner(System.in);
-      try
-      {
-         System.out.print("Enter an integer >> ");
-         x = input.nextInt();
-      }
-      catch(Exception e)
-      {
-         System.out.println("Exception occurred");
-      }  
-      System.out.println("x is " + x); 
-   }
+
+class UninitializedVariableTest {
+    static void main(String[] args) {
+        int x;
+        Scanner input = new Scanner(System.in);
+        try {
+            System.out.print("Enter an integer >> ");
+            x = input.nextInt();
+            System.out.println("x is " + x);
+        } catch (Exception e) {
+            System.out.println("Exception occurred");
+        }
+
+    }
 }
       
